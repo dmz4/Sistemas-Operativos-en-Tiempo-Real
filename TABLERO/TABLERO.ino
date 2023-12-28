af3186d4f1d3bb2b30328f392b52f9ba4b09acaf
dmz4 <dmz4alejandro@gmail.com>
2023-12-28 16:12:16 -0600

    Update TABLERO.ino

--- TABLERO/TABLERO.ino
+++ TABLERO/TABLERO.ino
@@ -1,11 +1,12 @@
 //Tablero de automovil con FREERTOS
+#include <ESP32Servo.h>
 #if CONFIG_FREERTOS_UNICORE
 static const BaseType_t app_cpu = 0;
 #else
 static const BaseType_t app_cpu = 1;
 #endif
 
-
+// Tareas
 static TaskHandle_t Tarea01 = NULL;
 static TaskHandle_t Tarea02 = NULL;
 static TaskHandle_t Tarea03 = NULL;
@@ -16,90 +17,271 @@ static TaskHandle_t Tarea07 = NULL;
 static TaskHandle_t Tarea08 = NULL;
 static TaskHandle_t Tarea09 = NULL;
 
+// Velocidad
+int velocidadKm = 0;
+int RPM = 0;
 
-//Tarea UART 01
+Servo vKm;
+Servo vRPM;
+
+// LED PARA TESTIGO DE TEMPERATURA
+#define TEMP_RED    23 
+#define TEMP_GREEN  22 
+#define TEMP_BLUE   21
+
+float temperature = 0;
+
+
+// LED PARA TESTIGO DE ACEITE
+#define ACEITE_RED    19 
+#define ACEITE_GREEN  18 
+#define ACEITE_BLUE   5
+
+float aceite = 0;
+
+
+// LED PARA TESTIGO DE AIRBAG
+#define AIRBAG_LED 17
+
+int sensor_airbag = 0; // Si es HIGH se activa el testigo
+
+
+// LED PARA TESTIGO DE CINTURON
+#define CINTO_LED 16
+
+int sensor_cinturon = 0; // Si es HIGH se activa el testigo
+
+// LED PARA TESTIGO DE PUERTAS
+#define DOOR_LED 4
+
+int sensor_puertas = 0; // Si es HIGH se enciende el testigo lo que indica que una puerta esta abierta
+
+
+// LED PARA TESTIGO DE COFRE
+#define COFRE_LED 0
+
+int sensor_cofre = 0; // Si es HIGH se enciende el testigo lo que indica que el cofre esta abierto
+
+// LED PARA TESTIGO DE CAJUELA
+#define CAJUELA_LED 2
+
+int sensor_cajuela = 0; // Si es HIGH se enciende el testigo
+
+// LEDs PARA INTERMITENTES Y DIRECCIONALES
+#define RIGHT 1
+#define LEFT 3
+int s_dir = 0; // Si es 0 estan apagadas, 1 si estan intermitentes, 2 direccionales derecha y 3 direccionales izquierda
+
+//Tarea testigo de aceite
 void Aceite(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Aceite");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    aceite = analogRead(39); // Nivel de aceite en porcentaje
+    
+    if(aceite > 10 && aceite <= 20) // Si el nivel de aceite esta ente 10% y 20& el testigo prende en amarillo
+    {
+      analogWrite(ACEITE_RED,   255);
+      analogWrite(ACEITE_GREEN, 255);
+      analogWrite(ACEITE_BLUE,  0);
+    }
+    if(aceite >= 0 && aceite <= 10) // Si el nivel de aceite es inferior a 10& el testigo prende en rojo
+    {
+      analogWrite(ACEITE_RED,   255);
+      analogWrite(ACEITE_GREEN, 0);
+      analogWrite(ACEITE_BLUE,  0);
+    }
   }
 }
 
-//Tarea UART 02
+//Tarea testigo airbag
 void Airbag(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Airbag");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    sensor_airbag = digitalRead(34); // Lectura de sensor de airbag
+    
+    if(sensor_airbag == HIGH)
+    {
+      digitalWrite(AIRBAG_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(AIRBAG_LED, LOW);
+    }
   }
 }
 
-//Tarea UART 03
+//Tarea testigo de temperatura
 void Temperatura(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Temperatura");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    temperature = analogRead(36); // Lectura de sensor de temperatura
+    
+    if(temperature >= 80 && temperature < 90) // Si la temperatura es mayor a 80 y menor a 90 el testigo prende de color amarillo
+    {
+      analogWrite(TEMP_RED,   255);
+      analogWrite(TEMP_GREEN, 255);
+      analogWrite(TEMP_BLUE,  0);
+    }
+    if(temperature >= 90 && temperature <= 100) // Si la temperatura es mayor a 90 y menor o igual a 100 el testigo prende de color rojo
+    {
+      analogWrite(TEMP_RED,   255);
+      analogWrite(TEMP_GREEN, 0);
+      analogWrite(TEMP_BLUE,  0);
+    }
+    
   }
 }
 
-//Tarea UART 04
+//Tarea testigo de cinturon
 void Cinturon(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Cinturon");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    velocidadKm = analogRead(13); // Lectura de velocidad en kilometros
+    sensor_cinturon = digitalRead(35); // Lectura de sensor de cinturon
+    
+    if(sensor_cinturon == HIGH && velocidadKm >= 20) // Si el sensor manda alto y la velocidad es mayor a 20 km/h
+    {
+      digitalWrite(CINTO_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(CINTO_LED, LOW);
+    }
   }
 }
 
-//Tarea UART 05
+//Tarea testigo de puertas
 void Puertas(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Puertas");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    sensor_puertas = digitalRead(32); // Lectura de sensor de puertas abiertas
+   
+    if(sensor_puertas == HIGH) // Si el sensor manda alto 
+    {
+      digitalWrite(DOOR_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(DOOR_LED, LOW);
+    }
   }
 }
 
-//Tarea UART 06
+//Tarea testigo de cofre
 void Cofre(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Cofre");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    sensor_cofre = digitalRead(33); // Lectura de sensor de puertas abiertas
+    
+    if(sensor_cofre == HIGH) // Si el sensor manda alto 
+    {
+      digitalWrite(COFRE_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(COFRE_LED, LOW);
+    }
   }
 }
 
-//Tarea UART 07
+//Tarea testigo de cajuela
 void Cajuela(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Cajuela");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    sensor_cajuela = digitalRead(25); // Lectura de sensor de puertas abiertas
+    
+    if(sensor_cajuela == HIGH) // Si el sensor manda alto 
+    {
+      digitalWrite(CAJUELA_LED, HIGH);
+    }
+    else
+    {
+      digitalWrite(CAJUELA_LED, LOW);
+    }
   }
 }
 
-//Tarea UART 08
-void Intermitentes(void*parameter){
+//Tarea intermitentes y direccionales
+void IntermitentesDIR(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Intermitentes");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+   
+    s_dir = analogRead(12); // Lectura de señal para intermitentes y direccionales
+    
+    if(s_dir == 0) // Si es 0 las luces se apagan
+    {
+      digitalWrite(RIGHT, LOW);
+      digitalWrite(LEFT, LOW);
+    }
+    else
+    {
+      if(s_dir == 1) // Si es 1 todas encienden de manera intermitente
+      {
+        digitalWrite(RIGHT, HIGH);
+        digitalWrite(LEFT, HIGH);
+        delay(100);
+        digitalWrite(RIGHT, LOW);
+        digitalWrite(LEFT, LOW);
+        delay(100);
+
+      }
+      else
+      {
+        if(s_dir == 2) // Si es 2 encienden las de la derecha
+        {
+          digitalWrite(RIGHT, HIGH);
+          digitalWrite(LEFT, LOW);
+          delay(100);
+          digitalWrite(RIGHT, LOW);
+          digitalWrite(LEFT, LOW);
+          delay(100);
+
+        }
+        else
+        {
+          if(s_dir == 3) // Si es 3 encienden las direccionales de la izquierda
+          {
+            digitalWrite(RIGHT, LOW);
+            digitalWrite(LEFT, HIGH);
+            delay(100);
+            digitalWrite(RIGHT, LOW);
+            digitalWrite(LEFT, LOW);
+            delay(100);
+          }
+
+        }
+        
+
+      }
+      
+
+    }
+    
   }
 }
 
-//Tarea UART 09
-void Dirreccionales(void*parameter){
+//Tarea indicadores de velocidad
+void Velocidad(void*parameter){
   while(1){
-    Serial.println();
-    Serial.println("Dirreccionales");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    velocidadKm = analogRead(13);
+    RPM = analogRead(14); 
+    //Hacemos regla de 3 para obtenr el angulo del servo respecto a la velocidad                                          
+    float anguloKm = velocidadKm * 180 / 261; // Maxima 260 km/h
+    float anguloRPM = RPM * 180 / 3001; // Maxima 3000 rpm
+    vKm.write(anguloKm); // Movemos los servomotores al angulo correspondiente
+    vRPM.write(anguloRPM);
   }
 }
 
 void setup() {
-  Serial.begin(300);
+  Serial.begin(9600);
+  pinMode(AIRBAG_LED, OUTPUT);
+  pinMode(TEMP_RED, OUTPUT);
+  pinMode(TEMP_GREEN, OUTPUT);
+  pinMode(TEMP_BLUE, OUTPUT);
+  pinMode(ACEITE_RED, OUTPUT);
+  pinMode(ACEITE_GREEN, OUTPUT);
+  pinMode(ACEITE_BLUE, OUTPUT);
+  pinMode(CINTO_LED, OUTPUT);
+  pinMode(DOOR_LED, OUTPUT);
+  pinMode(COFRE_LED, OUTPUT);
+  pinMode(CAJUELA_LED, OUTPUT);
+  pinMode(RIGHT, OUTPUT);
+  pinMode(LEFT, OUTPUT);
+  vKm.attach(27, 500, 2500);
+  vRPM.attach(26, 500, 2500);
   vTaskDelay(1000 / portTICK_PERIOD_MS);
     xTaskCreatePinnedToCore (
               Aceite,    // Funcion que se manda a llamar
@@ -172,7 +354,7 @@ void setup() {
   // put your setup code here, to run once:
 
     xTaskCreatePinnedToCore (
-              Intermitentes,    // Funcion que se manda a llamar
+              IntermitentesDIR,    // Funcion que se manda a llamar
               "Tarea08",    // El nombre de la tarea
               1024,           // Stack size
               NULL,           //
@@ -182,7 +364,7 @@ void setup() {
   // put your setup code here, to run once:
 
     xTaskCreatePinnedToCore (
-              Dirreccionales,    // Funcion que se manda a llamar
+              Velocidad,    // Funcion que se manda a llamar
               "Tarea09",    // El nombre de la tarea
               1024,           // Stack size
               NULL,           //
@@ -195,7 +377,6 @@ void setup() {
 
 void loop() {
 
-  vTaskDelay (500/ portTICK_PERIOD_MS);  
-  
-  */
+ 
 }
+
